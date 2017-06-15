cabe1bcce83b5b56c70c00983b610895eec0d356
Student01 <weerapatonline@gmail.com>
2017-06-15 17:02:33 +0700

    Check Space OK

--- My Service/AddnewuserViewController.swift
+++ My Service/AddnewuserViewController.swift
@@ -10,9 +10,44 @@ import UIKit
 
 class AddnewuserViewController: UIViewController {
     
-   
+    //Implicit
+    var nameString: String? = nil
+    var userString: String? = nil
+    var passwordString: String? = nil
     
+    
+   
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBOutlet weak var txtuser: UITextField!
   
+    @IBOutlet weak var txtpass: UITextField!
+    
+    
+    @IBAction func saveButton(_ sender: Any) {
+        //Get Value From TextField
+        nameString = txtName.text
+        userString = txtuser.text
+        passwordString = txtpass.text
+        
+        let intName = nameString?.characters.count
+        let intUser = userString?.characters.count
+        let intPassword = passwordString?.characters.count
+        
+        if (intName == 0) || (intUser == 0) || (intPassword == 0) {
+        
+        //Have Space
+            print("Have Space")
+        
+        }else{
+        print("No Space")
+        
+        }
+    
+    
+    }//saveButton
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
